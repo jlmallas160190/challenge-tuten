412d6e77ccbec18d175984728e58164db09037a2
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2021-03-30 16:16:55 -0500

    Adding config.

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks'],
+  rules: {
+    'no-underscore-dangle': 0,
+    'react/jsx-filename-extension': 'off',
+    'no-use-before-define': 'off',
+    'import/prefer-default-export': 0,
+    'no-console': 1,
+    'no-nested-ternary': 0,
+    'linebreak-style': 'off',
+    'comma-dangle': 'off',
+    'import/no-unresolved': 0,
+    'object-curly-newline': 'off',
+    'func-names': ['error', 'always', { generators: 'as-needed' }],
+    'import/no-useless-path-segments': 1,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-duplicates': 0,
+    'import/order': 0,
+    'import/newline-after-import': 1,
+    'import/no-named-as-default-member': 0,
+    'import/namespace': 0,
+    'import/named': 0,
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-array-index-key': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelComponents: ['CustomInputLabel'],
+        labelAttributes: ['label'],
+        controlComponents: ['CustomInput'],
+        depth: 3,
+      },
+    ],
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    window: true,
+    document: true,
+    navigator: true,
+    localStorage: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: 1,
+  },
+  settings: {
+    'import/extensions': 0,
+    'import/parser': 'babel-eslint',
+    'import/resolve': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+      moduleDirectory: ['node_modules', 'src'],
+    },
+  },
+};
---
+++ src/config/ProtectedRoute.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Route} from 'react-router-dom';
+
+function ProtectedRoute({ component: Component, permissions, ...rest }) {
+  const componentRender = () => Component;
+  const ComponentToRender = componentRender();
+  return <Route {...rest} render={(props) => <ComponentToRender {...props} />} />;
+}
+
+ProtectedRoute.propTypes = {
+  location: PropTypes.shape({}),
+  component: PropTypes.oneOfType([PropTypes.shape({}), PropTypes.func]),
+  permissions: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+};
+ProtectedRoute.defaultProps = {
+  location: {},
+  component: null,
+  permissions: [],
+};
+
+export default ProtectedRoute;
